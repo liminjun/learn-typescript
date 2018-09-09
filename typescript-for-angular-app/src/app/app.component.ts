d0d5e77df1504e1a8944543982ea62607b8a1937
liminjun <v-leeli@hotmail.com>
2018-09-09 22:07:53 +0800

    feat:模块和指令

--- typescript-for-angular-app/src/app/app.component.ts
+++ typescript-for-angular-app/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typescript-for-angular-app';
+
+  titleColor:string='red';
+  withTitle:boolean=true;
+  page:string="other";
+  currentDate:Date=new Date();
+
+  // name:string='';
+
+  // setName(event){
+  //   this.name=event.target.value;
+  // }
+
+  toggleTitleColor(){
+    if(this.titleColor==='red'){
+      this.titleColor='blue'
+    }else{
+      this.titleColor="red";
+    }
+  }
 }
--- typescript-for-angular-app/src/app/app.module.ts
+++ typescript-for-angular-app/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import {AuthService} from './auth.service';
+
+import { PlayerListComponent } from './player-list/player-list.component';
+import { FontSizeDirective } from './font-size.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    PlayerListComponent,
+    FontSizeDirective
   ],
+  exports:[MenuComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
